3e4531e300619bad4303093d241d36b7884c4a55
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-10-20 21:02:10 +0200

    added recyclerView

--- app/src/main/java/com/moringaschool/outingapi2/ApiArrayAdapter.java
+++ app/src/main/java/com/moringaschool/outingapi2/ApiArrayAdapter.java
@@ -1,26 +1,65 @@
 package com.moringaschool.outingapi2;
 
 import android.content.Context;
-import android.widget.ArrayAdapter;
-public class ApiArrayAdapter extends ArrayAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+import com.moringaschool.outingapi2.models.Event;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ApiArrayAdapter extends RecyclerView.Adapter<ApiArrayAdapter.RestaurantViewHolder> {
+    private List<Event> mEvents;
     private Context mContext;
-    private String[] mEvents;
 
-
-    public ApiArrayAdapter(Context mContext, int resource, String[] mEvents) {
-        super(mContext, resource);
-        this.mContext = mContext;
-        this.mEvents = mEvents;
-
-    }
-    @Override
-    public Object getItem(int position) {
-        String event = mEvents[position];
-        return event;
+    public ApiArrayAdapter(Context context, List<Event> events) {
+        mContext = context;
+        mEvents = events;
     }
 
     @Override
-    public int getCount() {
-        return mEvents.length;
+    public ApiArrayAdapter.RestaurantViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_each, parent, false);
+        RestaurantViewHolder viewHolder = new RestaurantViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ApiArrayAdapter.RestaurantViewHolder holder, int position) {
+        holder.bindRestaurant(mEvents.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mEvents.size();
+    }
+
+    public class RestaurantViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.eventImageView)
+        ImageView mEventImageView;
+        @BindView(R.id.eventNameTextView) TextView mEventNameTextView;
+        @BindView(R.id.categoryTextView) TextView mCategoryTextView;
+        @BindView(R.id.ratingTextView)
+        TextView mRatingTextView;
+        private Context mContext;
+
+        public RestaurantViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindRestaurant(Event event) {
+            mEventNameTextView.setText(event.getName());
+            mCategoryTextView.setText(event.getDescription());
+
+        }
     }
 }
